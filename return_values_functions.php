43f96412a9459176817826b9fe7ca1ac07bc38fb
David Weston Webb <westwebb@gmail.com>
2019-01-10 13:03:54 -0500

    Create return_values_functions.php

---
+++ return_values_functions.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+function addNumbers($number1, $number2){
+
+    $sum = $number1 + $number2;
+    return $sum;
+
+}
+
+$result = addNumbers(34,64);
+echo $result;
+// 98
+
+// echo "<br>";
+
+// I could also add this if I wanted to;
+// $result = addNumbers(100, $result);
+// echo $result;
+// 198
+
+// $result = addNumber(10000, $result);
+// echo $result;
+// 10198
+
+
+?>
+
+</body>
+</html>
